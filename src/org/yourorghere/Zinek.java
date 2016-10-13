4f74ebbcb29f402dacac0ffc8d6657e065ac810f
student <student@240-02.INFORMATYKA.local>
2016-10-13 13:53:11 +0200

    Lab3, zajecia 2, zad3

--- src/org/yourorghere/Zinek.java
+++ src/org/yourorghere/Zinek.java
@@ -217,7 +217,7 @@ gl.glVertex3f(-1.0f,-1.0f,-1.0f);
 gl.glVertex3f(1.0f,-1.0f,-1.0f);
 gl.glVertex3f(1.0f,-1.0f,1.0f);
 gl.glEnd();*/
-gl.glBegin(GL.GL_QUADS);
+/*gl.glBegin(GL.GL_QUADS);
 gl.glColor3f(1.0f,0.0f,1.0f);
 gl.glVertex3f(-1.0f,-1.0f,1.0f);
 gl.glVertex3f(-1.0f,-1.0f,-1.0f);
@@ -245,6 +245,47 @@ gl.glColor3f(1.0f,1.0f,0.0f);
 gl.glVertex3f(1.0f,-1.0f,1.0f);
 gl.glVertex3f(1.0f,-1.0f,-1.0f);
 gl.glVertex3f(0.0f,1.0f,0.0f);
+gl.glEnd();*/
+
+ 
+float x,y,kat;
+gl.glBegin(GL.GL_QUAD_STRIP);
+//gl.glVertex3f(0.0f,0.0f,-6.0f); //œrodek
+gl.glColor3f(1.0f,1.0f,0.0f);
+for(kat = 0.0f; kat < (2.0f*Math.PI);
+kat+=(Math.PI/32.0f))
+{
+x = 1.0f*(float)Math.sin(kat);
+y = 1.0f*(float)Math.cos(kat);
+gl.glVertex3f(x, 2.0f, y);
+gl.glVertex3f(x, -2.0f, y);//kolejne punkty
+}
+gl.glEnd();
+
+float xx,yy,katt;
+gl.glBegin(GL.GL_TRIANGLE_FAN);
+gl.glColor3f(1.0f,0.0f,0.0f);
+gl.glVertex3f(0.0f,2.0f,0.0f); //œrodek
+for(katt = 0.0f; katt < (2.0f*Math.PI);
+katt+=(Math.PI/32.0f))
+{
+xx = 1.0f*(float)Math.sin(katt);
+yy = 1.0f*(float)Math.cos(katt);
+gl.glVertex3f(xx, 2.0f, yy); //kolejne punkty
+}
+gl.glEnd();
+
+float xxx,yyy,kattt;
+gl.glBegin(GL.GL_TRIANGLE_FAN);
+gl.glColor3f(0.0f,1.0f,0.0f);
+gl.glVertex3f(0.0f,-2.0f,0.0f); //œrodek
+for(kattt = (float) (2.0f*Math.PI); kattt > 0.0f;
+kattt-=(Math.PI/32.0f))
+{
+xxx = 1.0f*(float)Math.sin(kattt);
+yyy = 1.0f*(float)Math.cos(kattt);
+gl.glVertex3f(xxx, -2.0f, yyy); //kolejne punkty
+}
 gl.glEnd();
 
  //Wykonanie wszystkich operacji znajduj¹cych siê w buforze
